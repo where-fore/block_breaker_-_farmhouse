ad15870118bfd0dcc97e93b74847fb2a4d1ba549
Dover <patrick_dover@yahoo.ca>
2019-05-10 15:50:12 -0400

    Added Score Counter

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -8,6 +8,13 @@ public class GameStatus : MonoBehaviour
 
     [SerializeField] [Range(0, 5)]
     private float gameSpeed = 1f;
+    
+    private ScoreTextManager scoreTextManager = null;
+
+    private void Start()
+    {
+        scoreTextManager = FindObjectOfType<ScoreTextManager>();
+    }
 
     private void Update()
     {
@@ -22,5 +29,6 @@ public class GameStatus : MonoBehaviour
     public void AddScore(int scoreToAdd)
     {
         currentScore = currentScore + scoreToAdd;
+        scoreTextManager.UpdateScoreTo(currentScore);
     }
 }
---
+++ Assets/Scripts/ScoreTextManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreTextManager : MonoBehaviour
+{
+    private int startingScore = 0;
+
+    private void Start()
+    {
+        UpdateScoreTo(startingScore);
+    }
+
+    public void UpdateScoreTo(int newScore)
+    {
+        GetComponent<TextMeshProUGUI>().text = newScore.ToString();
+    }
+}
